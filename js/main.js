8bdb36dbe898f7027fcc2917636c4acc393f4fb2
cameronthecoder <camnooten@gmail.com>
2021-01-15 13:21:23 -0600

    Make formatting changes

--- js/main.js
+++ js/main.js
@@ -1,56 +1,55 @@
-var input = document.querySelector('input');
-var form = document.querySelector('form');
-var alert = document.querySelector('.alert');
+var input = document.querySelector("input");
+var form = document.querySelector("form");
+var alert = document.querySelector(".alert");
 
 // Try to hook into Adobe Connect
 var cpu = ConnectCustomSDK.SyncConnector || {};
 
-
 // Check if we are actually in an Adobe Connect room
 
 if (cpu) {
-	const onConfigured = () => {
-		console.log('Custom pod is ready.')
-	}
+  const onConfigured = () => {
+    console.log("Custom pod is ready.");
+  };
 
-	const onSyncMessageReceived = (event) => {
-		// event.msgNm = name of message
-		// event.msgVal = value of message
+  const onSyncMessageReceived = (event) => {
+    // event.msgNm = name of message
+    // event.msgVal = value of message
 
-		alert.innerHTML = "Message received: " + event.msgVal + " (Event name: <code>" + event.msgNm + "</code>)";
-		alert.style.display = "block";
-	}
+    alert.innerHTML =
+      "Message received: " +
+      event.msgVal +
+      " (Event name: <code>" +
+      event.msgNm +
+      "</code>)";
+    alert.style.display = "block";
+  };
 
-	cpu.init(
-		onConfigured, 
-		"com.example.custompodexample",
-		"9.5.001",
-		"connectsdkhook"
-	);
+  cpu.init(
+    onConfigured,
+    "com.example.custompodexample",
+    "9.5.001",
+    "connectsdkhook"
+  );
 
-	// Allow participants to send a message event
-	cpu.allowParticipantPublish("message", true);
+  // Allow participants to send a message event
+  cpu.allowParticipantPublish("message", true);
 
-	// Register the syncMessageReceieved event
-	// This will allow us to receive a message from another person
-	cpu.registerCallback("syncMessageReceived", onSyncMessageReceived);
+  // Register the syncMessageReceieved event
+  // This will allow us to receive a message from another person
+  cpu.registerCallback("syncMessageReceived", onSyncMessageReceived);
 
-	// Check when form is submitted
-	form.addEventListener('submit', function(e) {
-		e.preventDefault();
+  // Check when form is submitted
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
 
-		console.log('Form submitted');
+    console.log("Form submitted");
 
-		alert.innerText = "Message sent!";
-		alert.style.display = "block";
+    alert.innerText = "Message sent!";
+    alert.style.display = "block";
 
-		// send a message to all of the people in the room except yourself
-		// input.value is the value of the message
-		cpu.dispatchSyncMessage(
-			"message",
-			input.value,
-			false,
-			false
-		);
-	});
+    // send a message to all of the people in the room except yourself
+    // input.value is the value of the message
+    cpu.dispatchSyncMessage("message", input.value, false, false);
+  });
 }
